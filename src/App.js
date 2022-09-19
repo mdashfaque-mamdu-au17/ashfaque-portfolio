0c560d9cf6a15a2301becafed49c553eda97b8a7
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-19 23:08:32 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h3 className="bg-cyan-300">Green</h3>
+    </div>
+  );
+}
+
+export default App;
