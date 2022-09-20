618db95968295a5b22c69988b253ed6781a7d0d3
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-21 01:52:43 +0530

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      colors: {
+        'theme-navy-blue': '#343D68',
+        'theme-red-600': '#E84949',
+      },
       fontFamily: {
         BeVietnamPro: ['Be Vietnam Pro', 'sans-serif'],
       },
