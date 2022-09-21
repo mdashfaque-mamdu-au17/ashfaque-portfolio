82395cb2f3ae7c53565a910e6236cdf3e87732ea
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-22 00:20:15 +0530

    colors and box shadow added

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,14 @@ module.exports = {
       colors: {
         'theme-navy-blue': '#343D68',
         'theme-red-600': '#E84949',
+        'theme-gray-400': '#838383',
       },
       fontFamily: {
         BeVietnamPro: ['Be Vietnam Pro', 'sans-serif'],
+      },
+      boxShadow: {
+        'custom-shadow':
+          '0px 2px 2px rgba(0, 0, 0, 0.14), 0px 3px 1px rgba(0, 0, 0, 0.12), 0px 1px 5px rgba(0, 0, 0, 0.2)',
       },
     },
   },
