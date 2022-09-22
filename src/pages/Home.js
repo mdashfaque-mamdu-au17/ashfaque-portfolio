8dfee909de75126487783b78b4ac033139fe2885
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-22 22:14:07 +0530

    skills section completed

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ProfileSection from '../components/ProfileSection';
+import SkillsSection from '../components/SkillsSection';
 
 const Home = () => {
   return (
-    <div className="-z-10">
+    <div className="z-10">
       <ProfileSection />
+      <SkillsSection />
     </div>
   );
 };
