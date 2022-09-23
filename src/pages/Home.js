5e5ca7cdf7000f6da462a194cbaf8d976366e662
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-24 00:54:25 +0530

    navbar placed in homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ProfileSection from '../components/ProfileSection';
+import ProjectsSection from '../components/ProjectsSection';
 import SkillsSection from '../components/SkillsSection';
-
+import Navbar from '../components/Navbar';
 const Home = () => {
   return (
-    <div className="z-10">
-      <ProfileSection />
-      <SkillsSection />
-    </div>
+    <>
+      <Navbar />
+      <div className="z-10">
+        <ProfileSection />
+        <SkillsSection />
+        <ProjectsSection />
+      </div>
+    </>
   );
 };
 
