9a7a2ce8ee37c689aadf778ca2268e52d7f58e2c
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-24 01:27:14 +0530

    contact me section started

---
+++ src/components/ContactSection.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ContactSection = () => {
+  return (
+    <section id="contact-me" className="pt-[121px] lg:pt-[160px]">
+      ContactSection
+    </section>
+  );
+};
+
+export default ContactSection;
