7bf848d1d0cb806d221d106dd348854be53926bb
mdashfaque-mamdu-au17 <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2022-09-25 00:13:46 +0530

    projects page completed

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,7 +1,47 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import ProjectCard from '../components/ProjectCard';
+import { allProjects } from '../components/projectdata';
 const Projects = () => {
-  return <div>Projects</div>;
+  return (
+    <>
+      <nav className="sticky top-0 z-30 px-8 pt-6 mx-auto bg-white lg:pt-10 max-w-7xl">
+        <div>
+          <Link
+            to="/"
+            className="text-xl font-bold lg:text-4xl text-theme-navy-blue"
+          >
+            ashfaque
+          </Link>
+        </div>
+      </nav>
+
+      <div>
+        <section className="mx-auto max-w-7xl pt-[105px] pb-[105px]">
+          <div className="z-10 pt-[43px] lg:pt-[30px] flex flex-wrap gap-y-[69px] justify-center gap-4 px-8 xl:grid xl:grid-cols-3 xl:flex-none gap-x-[58px]">
+            {allProjects?.map((project, index) => {
+              const {
+                id,
+                projectImage,
+                description,
+                deployedLink,
+                githubLink,
+              } = project;
+              return (
+                <ProjectCard
+                  key={id}
+                  imageSrc={projectImage}
+                  description={description}
+                  deployedLink={deployedLink}
+                  githubLink={githubLink}
+                />
+              );
+            })}
+          </div>
+        </section>
+      </div>
+    </>
+  );
 };
 
 export default Projects;
