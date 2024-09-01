1880d1f7ed1830b5aff346551fc4f292d6b2923c
Ashfaque <77242783+mdashfaque-mamdu-au17@users.noreply.github.com>
2024-09-01 17:55:31 +0530

    analytics and seo added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,45 @@ import './globals.css';
 import { Providers } from '@/lib/provider';
 import Header from '@/components/layout/header';
 import Footer from '@/components/layout/footer';
+import Script from 'next/script';
 
 const inter = Inter({ subsets: ['latin'] });
 
+const title = 'Ashfaque Mamdu | Frontend Developer From Kumta, India';
+const description =
+  'Frontend Developer specializing in React.js with experience in backend development using Node.js. Explore my portfolio to see my projects, skills, and expertise in creating dynamic and responsive web applications.';
+const url = 'https://ashfaque-portfolio.vercel.app/';
+
 export const metadata: Metadata = {
-  title: 'Ashfaque Mamdu | Frontend Developer From Kumta, India',
-  description:
-    'Frontend Developer specializing in React.js with experience in backend development using Node.js. Explore my portfolio to see my projects, skills, and expertise in creating dynamic and responsive web applications.',
+  metadataBase: new URL(url),
+  title,
+  description,
+  keywords: [
+    'Frontend Developer',
+    'Full Stack Developer',
+    'React Developer',
+    'Next.js Developer',
+  ],
+  creator: 'Ashfaque Mamdu',
+  themeColor: [
+    { media: '(prefers-color-scheme: light)', color: 'white' },
+    { media: '(prefers-color-scheme: dark)', color: 'black' },
+  ],
+  openGraph: {
+    type: 'website',
+    url,
+    title,
+    description,
+    siteName: title,
+    images: [
+      {
+        url: '/images/open-graph-ashfaque.png',
+      },
+    ],
+  },
 };
 
-// const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID;
+const googleAnalyticsId = process.env.GOOGLE_ANALYTICS_ID;
 
 export default function RootLayout({
   children,
@@ -22,6 +51,23 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth" suppressHydrationWarning>
+      {googleAnalyticsId ? (
+        <head>
+          <Script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${googleAnalyticsId}`}
+          ></Script>
+          <Script id="google-anayltics-script">
+            {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+          
+            gtag('config', '${googleAnalyticsId}');
+          `}
+          </Script>
+        </head>
+      ) : null}
       <body className={`${inter.className} bg-gray text-gray-600 antialiased`}>
         <Providers>
           <Header />
